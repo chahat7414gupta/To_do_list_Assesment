6c1272041d32ddb476b5f388b070384653032777
chahat gupta <105102632+chahat7414gupta@users.noreply.github.com>
2025-08-08 10:45:47 +0530

    Update urls.py

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='task_list'),  # renamed from 'index' to 'task_list'
+    path('', views.index, name='task_list'),  
     path('add/', views.add_task, name='add_task'),
     path('api/tasks/', views.api_tasks, name='api_tasks'),
     path('api/tasks/<int:task_id>/', views.api_task_detail, name='api_task_detail'),
